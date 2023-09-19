e681c88f1d96d15dff8c6dd3ca6d1d5e6c12f823
Tiger <tigominecraft@gmail.com>
2023-09-19 22:37:41 +0200

    generator stuff

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -7,41 +7,41 @@ public class MapGenerator : MonoBehaviour
 {
     [SerializeField] GameObject tileObject;
     List<GameObject> tiles = new List<GameObject>();
+    int numberOfTilesSpawned;
 
     [SerializeField] GameObject cameraObj;
+
+    Vector3 cameraTargetPosition;
 
     [SerializeField] Transform originTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Spawner());
+        InvokeRepeating("SpawnNewTile", 3, 0.25f);
     }
 
     private void FixedUpdate()
     {
-        cameraObj.transform.position = new Vector3(cameraObj.transform.position.x, transform.position.y, transform.position.z);
+        cameraObj.transform.position = new Vector3(cameraObj.transform.position.x, cameraObj.transform.position.y, cameraObj.transform.position.z + 1.5f);
     }
 
-    IEnumerator Spawner()
+    void SpawnNewTile()
     {
         Vector3 newTilePosition = originTransform.position;
         Quaternion rot = Quaternion.identity;
 
-        for (int i = 0; i < 250; i++)
-        {
-            newTilePosition = new Vector3(originTransform.position.x, originTransform.position.y, originTransform.position.z + (50 * i));
-            GameObject temp = Instantiate(tileObject, newTilePosition, rot);
-            tiles.Add(temp);
-            StartCoroutine(Despawn(temp));
+        newTilePosition = new Vector3(originTransform.position.x, originTransform.position.y, originTransform.position.z + (50 * numberOfTilesSpawned));
+        GameObject temp = Instantiate(tileObject, newTilePosition, rot);
+        tiles.Add(temp);
 
-            yield return new WaitForSeconds(0.1f);
-        }
+        numberOfTilesSpawned++;
+        StartCoroutine(Despawn(temp));
     }
 
     IEnumerator Despawn(GameObject obj)
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(10);
         Destroy(obj);
     }
 }
